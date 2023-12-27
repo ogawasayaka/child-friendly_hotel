512db747b43913b71167ab60ddfe123df776eab5
ogawasayaka <sayaka.i333@gmail.com>
2023-12-27 12:54:06 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+  get 'term', to: 'static_pages#term'
+  get 'privacy', to: 'static_pages#privacy'
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,21 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'],
     plugins: [require("daisyui")],
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+                "primary": "#f3f4f6",
+                "secondary": "#fca5a5",
+                "accent": "#ef4444",
+                "neutral": "#fee2e2",
+                "base-100": "#f5f5f4",
+                "info": "#ef4444",
+                "success": "#881337",
+                "warning": "#ea580c",
+                "error": "#fbbf24",
+          },
+        },
+      ],
+}
 }
