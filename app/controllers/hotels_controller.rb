070da1c56551322b550c1b54cd464e9bc9c6f925
ogawasayaka <sayaka.i333@gmail.com>
2024-01-08 14:58:38 +0900

    都道府県での検索フォーム作成、モデルの関連付け

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,12 @@
 class HotelsController < ApplicationController
+  def index
+    @hotels = Hotel.all
+    @prefectures = Prefecture.all
+    @q = Hotel.ransack(params[:q])
+  end
+
+  def search
+    @q = Hotel.ransack(params[:q])
+    @results = @q.result(distinct: true)
+  end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,10 @@
 class Hotel < ApplicationRecord
+  has_many :reviews
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "name", "name_url", "prefecture_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["prefecture"]
+  end
 end
