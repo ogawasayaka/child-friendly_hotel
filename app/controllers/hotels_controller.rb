c8f5344caaeeb396d101f375674a2536e8e84a79
ogawasayaka <sayaka.i333@gmail.com>
2024-01-13 13:20:33 +0900

    検索機能の実装【子の年齢】指定なし

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -9,10 +9,10 @@ class HotelsController < ApplicationController
     @q = Hotel.ransack(params[:q])
     @results = @q.result(distinct: true)
     reviews_age_eq = params[:q][:reviews_age_eq]
-    if reviews_age_eq.present?
-      @reviews = Review.joins(:hotel).merge(@results).where(age: reviews_age_eq)
-    else
+    if reviews_age_eq == 'on'
       @reviews = Review.joins(:hotel).merge(@results)
+    else
+      @reviews = Review.joins(:hotel).merge(@results).where(age: reviews_age_eq)
     end
   end
 end
