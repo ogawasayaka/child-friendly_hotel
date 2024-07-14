0ede74caabc50c7320f0aa13c72980e5cfa11ccf
ogawasayaka <sayaka.i333@gmail.com>
2024-07-14 22:31:00 +0900

    会員登録ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get 'term', to: 'static_pages#term'
   get 'privacy', to: 'static_pages#privacy'
+  get 'login', to: 'user_sessions#new'
+  post'logout', to: 'user_sessions#destroy'
 
   resources :hotels do
     collection do
       get 'search'
     end
   end
+
+  resources :users, only: %i[new create]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
