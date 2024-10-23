375a3be4264f7e0360f3d245675bcc5aa22b040d
ogawasayaka <sayaka.i333@gmail.com>
2024-10-24 04:59:45 +0900

    ログアウトのエラー解消

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // Entry point for the build script in your package.json
 import "./controllers"
+import "@hotwired/turbo-rails"
 import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'privacy', to: 'static_pages#privacy'
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
-  delete'/logout', to: 'user_sessions#destroy', as: :logout
+  delete '/logout', to: 'user_sessions#destroy', as: :logout
 
   resources :hotels do
     collection do
